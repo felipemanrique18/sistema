311742c68194d78f1ab8cec9aaec387a0c36ac8d
felipe manrique <felipemanrique018@gmail.com>
2020-06-19 17:15:53 -0500

    Commit inicial de Laravel sobre Heroku

--- config/app.php
+++ config/app.php
@@ -14,7 +14,6 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
